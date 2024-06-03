d1db2d90b4dbcf011f921676322a7da8bf6261a9
Joris van de Donk <90917667+jorisdon@users.noreply.github.com>
2024-06-03 20:17:53 +0200

    feat: Add packer build agent templates for Linux (Ubuntu Jammy 22.04, Amazon Linux 2023) (#46)

---
+++ assets/packer/build-agents/linux/install_octobuild.ubuntu.x86_64.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Install octobuild on Ubuntu, x86_64 only
+#  (octobuild does not seem to have packages available for aarch64 at the moment)
+# Requires common tools to be installed first.
+curl -1sLf 'https://dl.cloudsmith.io/public/octobuild/octobuild/setup.deb.sh' | sudo -E bash
+sudo apt-get -o DPkg::Lock::Timeout=180 update -y
+sudo NEEDRESTART_MODE=a DEBIAN_FRONTEND=noninteractive apt-get -o DPkg::Lock::Timeout=180 install -y octobuild
+sudo mkdir -p /etc/octobuild
