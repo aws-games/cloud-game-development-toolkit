75c508f0a23364cc011aadf8ee78fb9c44205740
Joris van de Donk <jorisdon@amazon.com>
2024-07-18 19:09:53 +0200

    update: pass FSx volume id as parameter to the 'delete_oldest_snapshot' pipeline, and validate that it exists prior to invoking the remainder of the pipeline

--- assets/jenkins-pipelines/delete_oldest_snapshot.groovy
+++ assets/jenkins-pipelines/delete_oldest_snapshot.groovy
@@ -4,16 +4,25 @@ pipeline {
             label 'linux'
         }
     }
-    environment {
-        FSX_WORKSPACE_VOLUME_ID = "fsvol-XXXXXXXXXXXXXXXXX"
+    parameters {
+        string(name: 'FSX_VOLUME_ID', defaultValue: '', description: 'FSx volume ID of the volume to delete the oldest snapshot from')
     }
     stages {
+        stage('Validate Pipeline') {
+            steps {
+                script {
+                    if (env.FSX_VOLUME_ID == null || env.FSX_VOLUME_ID.length() <= 0) {
+                        throw new Exception("FSX_VOLUME_ID parameter/environment variable not set.")
+                    }
+                }
+            }
+        }
         stage('Delete') {
             steps {
                     script {
                         env.source_path = sh(returnStdout: true, script:'''
                         # Get the latest available snapshot that was created more than 7 days ago
-                        SNAPSHOTID=\$(aws fsx describe-snapshots --filters "Name=volume-id,Values=$FSX_WORKSPACE_VOLUME_ID" --query "sort_by(Snapshots,&CreationTime)[?CreationTime<='$(date +%Y-%m-%dT23:59:59.999999+23:59 -d "7 days ago")' && Lifecycle == 'AVAILABLE']" --output json | jq -r '.[0].SnapshotId')
+                        SNAPSHOTID=\$(aws fsx describe-snapshots --filters "Name=volume-id,Values=$FSX_VOLUME_ID" --query "sort_by(Snapshots,&CreationTime)[?CreationTime<='$(date +%Y-%m-%dT23:59:59.999999+23:59 -d "7 days ago")' && Lifecycle == 'AVAILABLE']" --output json | jq -r '.[0].SnapshotId')
 
                         # Delete the snapshot, if found
                         if [ "$SNAPSHOTID" != "null" ]; then
