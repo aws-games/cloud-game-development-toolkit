2e877da26cdc10db7a05fd8b40015e584a124a6d
Greg O <84852800+GrzesiekO@users.noreply.github.com>
2024-07-26 13:04:18 +0200

    chore: update p4_configure.sh and module configs for Helix Core
    Updated the p4_configure.sh script to include a new function that appends
    the newly mounted EBS volumes or FSx mount points to the /etc/fstab file.
    This ensures the mounts persist across reboots.
    
    Also made some minor updates to the Terraform module configurations for
    the Helix Core resources to align with the script changes.

--- modules/perforce/helix-core/data.tf
+++ modules/perforce/helix-core/data.tf
@@ -5,12 +5,12 @@ data "aws_subnet" "instance_subnet" {
 # Fetching custom Perforce Helix Core AMI
 data "aws_ami" "helix_core_ami" {
   most_recent = true
-  name_regex  = "p4_rocky_linux-*"
+  name_regex  = "p4_al2023-*"
   owners      = ["self"]
 
   filter {
     name   = "name"
-    values = ["p4_rocky_linux-*"]
+    values = ["p4_al2023-*"]
   }
 
   filter {
