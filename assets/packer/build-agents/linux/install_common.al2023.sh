c43d0450b276e8d34dc7dc6a24f9f7ea6026ead4
Joris van de Donk <jorisdon@amazon.com>
2024-08-14 14:15:00 +0000

    fix: use `dnf` instead of `yum` to install packages on AL2023. This avoids a potential installation issue where packages can't be found in /var/cache or a lock can't be acquired.

--- assets/packer/build-agents/linux/install_common.al2023.sh
+++ assets/packer/build-agents/linux/install_common.al2023.sh
@@ -25,7 +25,7 @@ echo "Updating packages..."
 sudo yum update -y
 echo "Installing packages..."
 sudo yum -y groupinstall "Development Tools"
-sudo yum install -y awscli java-11-amazon-corretto-headless java-11-amazon-corretto-devel libarchive libarchive-devel unzip cmake python3 python3-pip python3-requests clang lld git openssl libcurl-devel openssl-devel uuid-devel zlib-devel pulseaudio-libs-devel jq freetype-devel libsndfile-devel python3 jq libX11-devel libXcursor-devel libXinerama-devel mesa-libGL-devel mesa-libGLU-devel libudev-devel libXi-devel libXrandr-devel dos2unix
+sudo dnf install -y awscli java-11-amazon-corretto-headless java-11-amazon-corretto-devel libarchive libarchive-devel unzip cmake python3 python3-pip python3-requests clang lld git openssl libcurl-devel openssl-devel uuid-devel zlib-devel pulseaudio-libs-devel jq freetype-devel libsndfile-devel python3 jq libX11-devel libXcursor-devel libXinerama-devel mesa-libGL-devel mesa-libGLU-devel libudev-devel libXi-devel libXrandr-devel dos2unix
 sudo pip install boto3 botocore scons
 if [ "$(uname -p)" == "x86_64" ]; then
     sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
