9f58033bf69f0ab818375be8cac2e5a0f229904e
Henry Kiem <henrykie@amazon.com>
2024-11-18 12:13:00 -0800

    fix: Helix Core EIP outputs optional if var.internal is true

--- modules/perforce/helix-core/outputs.tf
+++ modules/perforce/helix-core/outputs.tf
@@ -1,15 +1,15 @@
 output "helix_core_eip_private_ip" {
-  value       = aws_eip.helix_core_eip[0].private_ip
+  value       = var.internal ? null : aws_eip.helix_core_eip[0].private_ip
   description = "The private IP of your Helix Core instance."
 }
 
 output "helix_core_eip_public_ip" {
-  value       = aws_eip.helix_core_eip[0].public_ip
+  value       = var.internal ? null : aws_eip.helix_core_eip[0].public_ip
   description = "The public IP of your Helix Core instance."
 }
 
 output "helix_core_eip_id" {
-  value       = aws_eip.helix_core_eip[0].id
+  value       = var.internal ? null : aws_eip.helix_core_eip[0].id
   description = "The ID of the Elastic IP associated with your Helix Core instance."
 }
 
@@ -19,12 +19,16 @@ output "security_group_id" {
 }
 
 output "helix_core_super_user_username_secret_arn" {
-  value       = var.helix_core_super_user_username_secret_arn == null ? awscc_secretsmanager_secret.helix_core_super_user_username[0].secret_id : var.helix_core_super_user_username_secret_arn
+  value = (var.helix_core_super_user_username_secret_arn == null ?
+    awscc_secretsmanager_secret.helix_core_super_user_username[0].secret_id :
+  var.helix_core_super_user_username_secret_arn)
   description = "The ARN of the AWS Secrets Manager secret holding your Helix Core super user's username."
 }
 
 output "helix_core_super_user_password_secret_arn" {
-  value       = var.helix_core_super_user_password_secret_arn == null ? awscc_secretsmanager_secret.helix_core_super_user_password[0].secret_id : var.helix_core_super_user_password_secret_arn
+  value = (var.helix_core_super_user_password_secret_arn == null ?
+    awscc_secretsmanager_secret.helix_core_super_user_password[0].secret_id :
+  var.helix_core_super_user_password_secret_arn)
   description = "The ARN of the AWS Secrets Manager secret holding your Helix Core super user's password."
 }
 
