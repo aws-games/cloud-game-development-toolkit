02ac46a4a3bcc358645a1e0b7c48c123a24f0bbd
Henry Kiem <henrykie@amazon.com>
2024-12-12 15:46:42 -0800

    feat(Perforce Example): Update complete example for shared networking configuration across services

--- modules/perforce/examples/complete/dns.tf
+++ modules/perforce/examples/complete/dns.tf
@@ -1,4 +1,3 @@
-
 ##########################################
 # Route53 Hosted Zone for FQDN
 ##########################################
@@ -8,10 +7,10 @@ data "aws_route53_zone" "root" {
 }
 
 ##########################################
-# Perforce Helix DNS
+# Perforce DNS
 ##########################################
-resource "aws_route53_zone" "helix_private_zone" {
-  name = "helix.perforce.internal"
+resource "aws_route53_zone" "perforce_private_hosted_zone" {
+  name = "perforce.${data.aws_route53_zone.root.name}"
   #checkov:skip=CKV2_AWS_38: Hosted zone is private (vpc association)
   #checkov:skip=CKV2_AWS_39: Query logging disabled by design
   vpc {
@@ -19,54 +18,57 @@ resource "aws_route53_zone" "helix_private_zone" {
   }
 }
 
-
-resource "aws_route53_record" "helix_swarm" {
+# Route all external web service traffic to the NLB
+resource "aws_route53_record" "external_perforce_web_services" {
   zone_id = data.aws_route53_zone.root.id
-  name    = "swarm.helix.${data.aws_route53_zone.root.name}"
+  name    = "*.perforce.${data.aws_route53_zone.root.name}"
   type    = "A"
   alias {
-    name                   = module.perforce_helix_swarm.alb_dns_name
-    zone_id                = module.perforce_helix_swarm.alb_zone_id
+    name                   = aws_lb.perforce.dns_name
+    zone_id                = aws_lb.perforce.zone_id
     evaluate_target_health = true
   }
 }
 
-resource "aws_route53_record" "helix_authentication_service" {
-  zone_id = data.aws_route53_zone.root.zone_id
-  name    = "auth.helix.${data.aws_route53_zone.root.name}"
+# Route all internal web service traffic to the ALB
+resource "aws_route53_record" "internal_perforce_web_services" {
+  zone_id = aws_route53_zone.perforce_private_hosted_zone.id
+  name    = "*.${aws_route53_zone.perforce_private_hosted_zone.name}"
   type    = "A"
   alias {
-    name                   = module.perforce_helix_authentication_service.alb_dns_name
-    zone_id                = module.perforce_helix_authentication_service.alb_zone_id
+    name                   = aws_lb.perforce_web_services.dns_name
+    zone_id                = aws_lb.perforce_web_services.zone_id
     evaluate_target_health = true
   }
 }
 
-resource "aws_route53_record" "perforce_helix_core" {
+# Route all external Helix Core traffic to the NLB
+resource "aws_route53_record" "external_helix_core" {
   zone_id = data.aws_route53_zone.root.zone_id
-  name    = "core.helix.${data.aws_route53_zone.root.name}"
+  name    = "perforce.${data.aws_route53_zone.root.name}"
   type    = "A"
-  ttl     = 300
-  #checkov:skip=CKV2_AWS_23:The attached resource is managed by CGD Toolkit
-  records = [module.perforce_helix_core.helix_core_eip_public_ip]
+  alias {
+    name                   = aws_lb.perforce.dns_name
+    zone_id                = aws_lb.perforce.zone_id
+    evaluate_target_health = true
+  }
 }
 
-resource "aws_route53_record" "perforce_helix_core_pvt" {
-  zone_id = aws_route53_zone.helix_private_zone.zone_id
-  name    = "core.${aws_route53_zone.helix_private_zone.name}"
+# Route all internal Helix Core traffic to the instance
+resource "aws_route53_record" "internal_helix_core" {
+  zone_id = aws_route53_zone.perforce_private_hosted_zone.zone_id
+  name    = aws_route53_zone.perforce_private_hosted_zone.name
   type    = "A"
+  records = [module.perforce_helix_core.helix_core_private_ip]
   ttl     = 300
-  #checkov:skip=CKV2_AWS_23:The attached resource is managed by CGD Toolkit
-  records = [module.perforce_helix_core.helix_core_eip_private_ip]
 }
 
 ##########################################
 # Helix Certificate Management
 ##########################################
-
-resource "aws_acm_certificate" "helix" {
-  domain_name               = "helix.${var.root_domain_name}"
-  subject_alternative_names = ["*.helix.${var.root_domain_name}"]
+resource "aws_acm_certificate" "perforce" {
+  domain_name               = "perforce.${var.root_domain_name}"
+  subject_alternative_names = ["*.perforce.${var.root_domain_name}"]
 
   validation_method = "DNS"
 
@@ -79,9 +81,9 @@ resource "aws_acm_certificate" "helix" {
   }
 }
 
-resource "aws_route53_record" "helix_cert" {
+resource "aws_route53_record" "perforce_cert" {
   for_each = {
-    for dvo in aws_acm_certificate.helix.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.perforce.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -96,10 +98,10 @@ resource "aws_route53_record" "helix_cert" {
   zone_id         = data.aws_route53_zone.root.id
 }
 
-resource "aws_acm_certificate_validation" "helix" {
+resource "aws_acm_certificate_validation" "perforce" {
   timeouts {
     create = "15m"
   }
-  certificate_arn         = aws_acm_certificate.helix.arn
-  validation_record_fqdns = [for record in aws_route53_record.helix_cert : record.fqdn]
+  certificate_arn         = aws_acm_certificate.perforce.arn
+  validation_record_fqdns = [for record in aws_route53_record.perforce_cert : record.fqdn]
 }
---
+++ modules/perforce/examples/complete/outputs.tf
@@ -0,0 +1,14 @@
+output "helix_core_connection_string" {
+  value       = "ssl:perforce.${var.root_domain_name}:1666"
+  description = "The connection string for the Helix Core server. Set your P4PORT environment variable to this value."
+}
+
+output "helix_swarm_url" {
+  value       = "swarm.perforce.${var.root_domain_name}"
+  description = "The URL for the Helix Swarm server."
+}
+
+output "helix_authentication_service_admin_url" {
+  value       = "auth.perforce.${var.root_domain_name}/admin"
+  description = "The URL for the Helix Authentication Service admin page."
+}
