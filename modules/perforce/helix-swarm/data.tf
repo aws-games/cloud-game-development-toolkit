4e4d35b02d0bb8d20cae082cf52202661b927ba6
Kevon Mayers <novekm@amazon.com>
2025-01-16 23:43:31 -0500

    add data source to fetch current AWS caller identity

--- modules/perforce/helix-swarm/data.tf
+++ modules/perforce/helix-swarm/data.tf
@@ -1,5 +1,9 @@
 data "aws_region" "current" {}
 
+# Fetch AWS caller identity (i.e. the AWS user or role that Terraform is authenticated as)
+data "aws_caller_identity" "current" {
+}
+
 # If cluster name is provided use a data source to access existing resource
 data "aws_ecs_cluster" "helix_swarm_cluster" {
   count        = var.cluster_name != null ? 1 : 0
