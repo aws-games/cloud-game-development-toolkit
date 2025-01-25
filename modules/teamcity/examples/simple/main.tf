be61d067e458cab54490b65262042e1dfaf6eff4
cwwalb <119454051+cwwalb@users.noreply.github.com>
2025-01-24 18:20:37 -0600

    initial TeamCity module without supporting resources

--- modules/teamcity/examples/simple/main.tf
+++ modules/teamcity/examples/simple/main.tf
@@ -1,6 +1,8 @@
 module "teamcity" {
-    source = "../../"
-    vpc_id = aws_vpc.teamcity_vpc.id
-    service_subnets = aws_subnet.private_subnets[*].id
+  source              = "../../"
+  vpc_id              = aws_vpc.teamcity_vpc.id
+  service_subnets     = aws_subnet.private_subnets[*].id
+  alb_subnets         = aws_subnet.public_subnets[*].id
+  alb_certificate_arn = aws_acm_certificate.teamcity.arn
 
 }
