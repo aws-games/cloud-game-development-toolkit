99ff32eb9ecfe3da0ebd38be27332b7f163a8456
danielw-aws <danieljw@amazon.com>
2025-02-06 12:53:45 -0800

    feat: improve sanity check script output readability

--- samples/unreal-cloud-ddc-single-region/assets/sanity_check.sh
+++ samples/unreal-cloud-ddc-single-region/assets/sanity_check.sh
@@ -10,8 +10,17 @@ unreal_cloud_ddc_nlb_dns_name=$(aws elbv2 describe-load-balancers --names unreal
 # Query AWS for the bearer token secret value and save it to a local variable
 bearer_token_secret_value=$(aws secretsmanager get-secret-value --secret-id unreal-cloud-ddc-token --query 'SecretString' --output text)
 
+echo "********************"
+echo "*Putting test data:*"
+echo "********************"
+echo ""
 # Curl the Cloud DDC API to PUT a "test" data blob
 curl http://$unreal_cloud_ddc_nlb_dns_name/api/v1/refs/ddc/default/00000000000000000000000000000000000000aa -X PUT --data 'test' -H 'content-type: application/octet-stream' -H 'X-Jupiter-IoHash: 4878CA0425C739FA427F7EDA20FE845F6B2E46BA' -i -H "Authorization: ServiceAccount $bearer_token_secret_value"
-
+echo ""
+echo ""
+echo "********************"
+echo "*Getting test data:*"
+echo "********************"
+echo ""
 # Curl the Cloud DDC API to GET the "test" data blob back
 curl http://$unreal_cloud_ddc_nlb_dns_name/api/v1/refs/ddc/default/00000000000000000000000000000000000000aa.json -i -H "Authorization: ServiceAccount $bearer_token_secret_value"
