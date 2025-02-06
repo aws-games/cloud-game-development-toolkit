b6a598b45bcc963922625c00300b0677a4dc4870
danielw-aws <danieljw@amazon.com>
2025-02-05 22:22:49 -0800

    refactor(unreal-cloud-ddc): improve scylla cluster configuration with seed nodes
    Splits scylla EC2 instances into seed and other nodes for better cluster initialization. Removes SSM-based configuration in favor of user data configuration. Updates scylla cluster configuration to use the seed node's IP for cluster formation. Updates output to include both seed and other nodes IPs.

--- modules/unreal/unreal-cloud-ddc-infra/locals.tf
+++ modules/unreal/unreal-cloud-ddc-infra/locals.tf
@@ -16,14 +16,26 @@ locals {
   scylla_variables = {
     scylla-cluster-name = var.name
   }
-  scylla_user_data = jsonencode(
+  scylla_user_data_primary_node = jsonencode(
     {
       "scylla_yaml" : {
         "cluster_name" : local.scylla_variables.scylla-cluster-name
       }
       #required to ensure that scylla does not pick up the wrong config on boot prior to SSM configuring the instance
       #if scylla boots with an ip that is incorrect you have to delete data and reset the node prior to reconfiguring.
-      "start_scylla_on_first_boot" : false
+      "start_scylla_on_first_boot" : true
+    }
+  )
+  scylla_user_data_other_nodes = jsonencode(
+    {
+      "scylla_yaml" : {
+        "cluster_name" : local.scylla_variables.scylla-cluster-name,
+        "seed_provider" : [{
+        "parameters" : [{ "seeds" : aws_instance.scylla_ec2_instance_seed[0].private_ip }] }]
+      }
+      #required to ensure that scylla does not pick up the wrong config on boot prior to SSM configuring the instance
+      #if scylla boots with an ip that is incorrect you have to delete data and reset the node prior to reconfiguring.
+      "start_scylla_on_first_boot" : true
     }
   )
   nvme-pre-bootstrap-userdata = <<EOF
