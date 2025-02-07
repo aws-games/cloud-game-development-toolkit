4c7568c320948adcb77b796efe366cdadde05fd9
danielw-aws <85375469+danielw-aws@users.noreply.github.com>
2025-02-06 16:00:18 -0800

    Feature: Unreal Cloud DDC Module and Examples (#341)
    Co-authored-by: Henry Kiem <henrykie@amazon.com>

---
+++ modules/unreal/unreal-cloud-ddc-intra-cluster/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = ">= 1.10.3"
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">=2.33.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">=2.16.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.73.0"
+    }
+  }
+}
