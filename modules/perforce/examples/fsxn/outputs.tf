d2a4696d971853e9896374f2dbd6c5ba0779c9ab
LirazRom10 <156958622+LirazRom10@users.noreply.github.com>
2025-02-16 17:15:57 +0200

    add resources to sample

--- modules/perforce/examples/fsxn/outputs.tf
+++ modules/perforce/examples/fsxn/outputs.tf
@@ -1,6 +1,19 @@
-
-
 output "amazon_fsxn_filesystem" {
   value       = aws_fsx_ontap_file_system.helix_core_fs.id
   description = "FSxN filesystem ID"
+}
+
+output "helix_core_connection_string" {
+  value       = "ssl:perforce.${var.root_domain_name}:1666"
+  description = "The connection string for the Helix Core server. Set your P4PORT environment variable to this value."
+}
+
+output "helix_swarm_url" {
+  value       = "https://swarm.perforce.${var.root_domain_name}"
+  description = "The URL for the Helix Swarm server."
+}
+
+output "helix_authentication_service_admin_url" {
+  value       = "https://auth.perforce.${var.root_domain_name}/admin"
+  description = "The URL for the Helix Authentication Service admin page."
 }
