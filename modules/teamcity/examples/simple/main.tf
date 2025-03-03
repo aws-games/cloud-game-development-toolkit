5b3e33234ea6f778d245d113d54bcb025e89a4bd
cwwalb <119454051+cwwalb@users.noreply.github.com>
2025-03-03 16:58:36 -0600

    revert changes for terraform destroy

--- modules/teamcity/examples/simple/main.tf
+++ modules/teamcity/examples/simple/main.tf
@@ -1,10 +1,7 @@
 module "teamcity" {
-  source                     = "../../"
-  vpc_id                     = aws_vpc.teamcity_vpc.id
-  service_subnets            = aws_subnet.private_subnets[*].id
-  alb_subnets                = aws_subnet.public_subnets[*].id
-  alb_certificate_arn        = aws_acm_certificate.teamcity.arn
-  database_connection_string = var.database_connection_string
-  database_master_username   = var.database_master_username
-  database_master_password   = var.database_master_password
+  source              = "../../"
+  vpc_id              = aws_vpc.teamcity_vpc.id
+  service_subnets     = aws_subnet.private_subnets[*].id
+  alb_subnets         = aws_subnet.public_subnets[*].id
+  alb_certificate_arn = aws_acm_certificate.teamcity.arn
 }
--- modules/teamcity/examples/simple/variables.tf
+++ modules/teamcity/examples/simple/variables.tf
@@ -1,15 +1,1 @@
-variable "database_connection_string" {
-  type        = string
-  description = "The database connection string for TeamCity"
-}
-
-variable "database_master_username" {
-  type        = string
-  description = "The master username for the database"
-}
-
-variable "database_master_password" {
-  type        = string
-  description = "The master password for the database"
-}
 
