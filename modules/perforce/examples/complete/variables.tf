04642318239ed301c22d2dc3423ff7be60e4f141
Kyle Somers <17753062+kylesomers@users.noreply.github.com>
2025-05-04 20:36:45 +0000

    feat: add optional cidr allowlist variable to perforce complete example

--- modules/perforce/examples/complete/variables.tf
+++ modules/perforce/examples/complete/variables.tf
@@ -2,3 +2,17 @@ variable "root_domain_name" {
   type        = string
   description = "The root domain name you would like to use for DNS."
 }
+
+
+variable "enable_private_access_perforce" {
+  type = object({
+    enabled = bool
+    cidr    = string
+  })
+  default     = null
+  description = "Enable private access to Perforce and specify allowlisted CIDR range."
+  validation {
+    condition     = var.enable_private_access_perforce.enabled == true && var.enable_private_access_perforce.cidr != null
+    error_message = "If private access to Perforce is enabled, a CIDR range must be provided."
+  }
+}
