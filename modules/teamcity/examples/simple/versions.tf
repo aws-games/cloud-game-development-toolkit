1580de733376910c7ee7d46543448a728ff79a19
Ayata Bernhardt <ayatanb@amazon.com>
2025-05-06 11:41:01 -0700

    adding support for creating a Project for TeamCity through terraform

--- modules/teamcity/examples/simple/versions.tf
+++ modules/teamcity/examples/simple/versions.tf
@@ -10,5 +10,20 @@ terraform {
       source  = "hashicorp/random"
       version = "3.5.1"
     }
+
+    # TESTING THIS TEAMCITY PROVIDER
+    teamcity = {
+      source = "jetbrains/teamcity"
+    }
   }
+}
+
+provider "teamcity" {
+  # host  = "http://localhost:8111"
+  host = "https://teamcity.ayatanb.people.aws.dev"
+  password = "6909622053639106778"
+}
+
+resource "teamcity_global_settings" "server_config" {
+  root_url = "teamcity.ayatanb.people.aws.dev"
 }
