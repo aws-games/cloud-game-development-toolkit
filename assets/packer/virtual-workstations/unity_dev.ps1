1f1839972c2284eb9cc8a3591a0d1672272cd4ce
Kevon Mayers <novekm@amazon.com>
2025-06-05 09:46:35 -0400

    Add PS scripts for virtual workstation AMI

---
+++ assets/packer/virtual-workstations/unity_dev.ps1
@@ -0,0 +1,6 @@
+# Use this file to write PowerShell script that installs things for Unity Development.
+# Once done, link this file in `userdata.ps1` so it will be used during Packer AMI creation.
+
+# Install Unity.
+
+#...etc
