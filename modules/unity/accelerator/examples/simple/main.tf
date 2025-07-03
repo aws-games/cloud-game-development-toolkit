7d9bab636ab315e7ab049de72dd5d05a018ffce1
Erik Ynigo <erikyb@amazon.com>
2025-07-03 13:44:38 -0700

    Added option to auto-generate dashboard username/password in Secrets Manager.

--- modules/unity/accelerator/examples/simple/main.tf
+++ modules/unity/accelerator/examples/simple/main.tf
@@ -4,6 +4,4 @@ module "unity_accelerator" {
   service_subnets     = aws_subnet.private_subnets[*].id
   lb_subnets          = aws_subnet.public_subnets[*].id
   alb_certificate_arn = aws_acm_certificate.unity_accelerator.arn
-  # unity_accelerator_dashboard_username_arn = "arn:aws:secretsmanager:us-east-2:637423293148:secret:dashboard-username-ainlYO"
-  # unity_accelerator_dashboard_password_arn = "arn:aws:secretsmanager:us-east-2:637423293148:secret:test-pQL2a3"
 }
