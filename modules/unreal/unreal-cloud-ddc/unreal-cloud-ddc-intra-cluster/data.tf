ac32cf3f4e2b98da1d297a800dde826551b1de6b
cwwalb <cwwalb@amazon.com>
2025-07-16 16:53:08 -0500

    multi region deploys and functional. single region sample updated

--- modules/unreal/unreal-cloud-ddc/unreal-cloud-ddc-intra-cluster/data.tf
+++ modules/unreal/unreal-cloud-ddc/unreal-cloud-ddc-intra-cluster/data.tf
@@ -1,12 +1,12 @@
-data "aws_region" "current" {
-  region = var.region
-}
+# data "aws_region" "current" {
+#   region = var.region
+# }
 data "aws_caller_identity" "current" {}
 
-data "aws_eks_cluster" "unreal_cloud_ddc_cluster" {
-  region = var.region
-  name   = var.cluster_name
-}
+# data "aws_eks_cluster" "unreal_cloud_ddc_cluster" {
+#   region = var.region
+#   name   = var.cluster_name
+# }
 
 data "aws_s3_bucket" "unreal_cloud_ddc_bucket" {
   region = var.region
@@ -18,7 +18,7 @@ data "aws_iam_openid_connect_provider" "oidc_provider" {
 }
 
 data "aws_lb" "unreal_cloud_ddc_load_balancer" {
-  depends_on = [helm_release.unreal_cloud_ddc]
+  depends_on = [helm_release.unreal_cloud_ddc_initialization]
   name       = "cgd-unreal-cloud-ddc"
   region     = var.region
 }
