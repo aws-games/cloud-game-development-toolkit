fed796854838d8ca75cd915d8da2ac7202a64eba
cwwalb <cwwalb@amazon.com>
2025-08-19 13:34:23 -0500

    fix(unreal cloud ddc): Unreal Cloud DDC single region sample     - updated the unreal cloud ddc single region example to be compatible with refactored unreal cloud ddc modules

--- samples/unreal-cloud-ddc-single-region/local.tf
+++ samples/unreal-cloud-ddc-single-region/local.tf
@@ -11,6 +11,19 @@ data "aws_caller_identity" "current" {}
 locals {
   azs = slice(data.aws_availability_zones.available.names, 0, 2)
 
+  vpc_cidr              = "192.168.0.0/16"
+  private_subnets_cidrs = ["192.168.0.0/24", "192.168.1.0/24"]
+  public_subnets_cidrs  = ["192.168.2.0/24", "192.168.3.0/24"]
+
+  nvme_managed_node_instance_type   = "i3en.xlarge"
+  worker_managed_node_instance_type = "c6i.large"
+  system_managed_node_instance_type = "m7i.large"
+
+  scylla_ami_name      = "ScyllaDB 6.2.1"
+  scylla_architecture  = "x86_64"
+  scylla_instance_type = "i4i.xlarge"
+
+
   tags = {
     Environment = "cgd"
     Application = "unreal-cloud-ddc"
