e1b11042bd43541de9f43ab1daeb8126212336ec
cwwalb <cwwalb@amazon.com>
2025-08-28 12:47:16 -0500

    update(unreal cloud ddc): Updated unreal cloud DDC Single Region sample

--- modules/unreal/unreal-cloud-ddc/examples/single-region/local.tf
+++ modules/unreal/unreal-cloud-ddc/examples/single-region/local.tf
@@ -1,5 +1,10 @@
 data "aws_availability_zones" "available" {
-  exclude_zone_ids = ["usw2-lax1-az1", "usw2-lax1-az2", "usw2-hnl1-az1", "usw2-las1-az1", "usw2-den1-az1"]
+  region = data.aws_region.current
+  # Filter for standard AZs only
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
 }
 
 data "aws_region" "current" {}
@@ -11,6 +16,19 @@ locals {
   # Networking
   public_subnet_cidrs  = ["192.168.2.0/24", "192.168.3.0/24"]
   private_subnet_cidrs = ["192.168.0.0/24", "192.168.1.0/24"]
+
+  vpc_cidr              = "192.168.0.0/16"
+  private_subnets_cidrs = ["192.168.0.0/24", "192.168.1.0/24"]
+  public_subnets_cidrs  = ["192.168.2.0/24", "192.168.3.0/24"]
+
+  nvme_managed_node_instance_type   = "i3en.xlarge"
+  worker_managed_node_instance_type = "c6i.large"
+  system_managed_node_instance_type = "m7i.large"
+
+  scylla_ami_name      = "ScyllaDB 6.2.1"
+  scylla_architecture  = "x86_64"
+  scylla_instance_type = "i4i.xlarge"
+
 
   tags = {
     Environment = "dev"
