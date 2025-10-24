79327d9dca1e6430f46e05cd4120500e876fcdcc
Gabriel Batista <141974958+gabebatista@users.noreply.github.com>
2025-10-24 11:21:34 -0400

    fix(unity-license): Adds conditional creation of sg for ENIs when using existing (#769)

--- modules/unity/accelerator/examples/simple/versions.tf
+++ modules/unity/accelerator/examples/simple/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.9"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.6.0"
+      version = "~> 6.6"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.7.2"
+      version = "~> 3.5"
     }
     http = {
       source  = "hashicorp/http"
